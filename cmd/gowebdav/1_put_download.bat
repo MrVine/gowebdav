c8d2b2284e7f1584e9f535dc79a943dd3895a0df
vitalii <porshegt4@gmail.com>
2018-07-17 16:43:14 +0300

    useful .bat files was added
    .bat file for PUT and RM was added

---
+++ cmd/gowebdav/1_put_download.bat
@@ -0,0 +1,2 @@
+@echo off
+gowebdav.exe -X PUT /wduser1/download/file.txt F:\temp\file.txt
---
+++ cmd/gowebdav/1_rm_upload.bat
@@ -0,0 +1,3 @@
+@echo off
+gowebdav.exe -X DEL /wduser1/upload/wduser2/some_file.txt
+gowebdav.exe -X DEL /wduser1/upload/wduser2/
